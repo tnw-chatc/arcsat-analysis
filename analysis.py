606af235650fc30dba04ca0b65b9e43edf23ca91
Tanawan (Juneau) Chatchadanoraset <tchatc24@uw.edu>
2025-06-10 20:25:18 -0700

    Plot light curve and periodogram

---
+++ analysis.py
@@ -0,0 +1,68 @@
+import numpy as np
+import polars
+import seaborn
+import glob
+import gc
+import matplotlib.pyplot as plt
+
+from astropy.timeseries import LombScargle
+import astropy.units as u
+
+from scipy.fft import rfft
+
+
+def plot_light_curve(times, fluxes):
+    """Plot the light curve obtained from the reduction process"""
+
+    # Convert JD to MJD
+    times_mjd = np.asarray(times) - 2400000.5
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    ax.scatter(times_mjd, fluxes)
+    ax.set_xlabel("time")
+    ax.set_ylabel("Relative Flux")
+    ax.grid(linestyle=":", alpha=0.5)
+    
+    fig.savefig("figures/light_curve.png")
+
+    # Clear
+    plt.clf()
+
+
+def determine_lc_period(times, fluxes, plot=False):
+
+    frequency, power = LombScargle(times, fluxes).autopower()
+
+    best_freq = frequency[np.argmax(power)]
+
+    period = 1/best_freq
+
+    # The actual period is twice the period of the periodiogram
+    lc_period = period.to(u.h) * 2 # type: ignore
+
+    if plot:
+        fig, ax = plt.subplots(figsize=(8,6))
+
+        ax.plot(frequency, power)
+        ax.axvline(x=best_freq.value, label=period, c='orange')
+        ax.set_xlabel("Freq (1/day)")
+        ax.set_title(f"System period: {lc_period}")
+        ax.set_xlim(0, 30)
+        ax.legend()
+        
+        fig.savefig("figures/periodogram.png")
+        
+    return lc_period
+
+
+if __name__ == "__main__":
+
+    # Load the datas
+    times = np.load("times.npy") * u.day # type: ignore
+    fluxes = np.load("fluxes.npy")    
+
+    plot_light_curve(times, fluxes)
+
+    lc_period = determine_lc_period(times, fluxes, plot=True)
+    print(lc_period)
