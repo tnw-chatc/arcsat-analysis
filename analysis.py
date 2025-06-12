d21cd225caa3765d2a0e5da618b8d5acd466da9a
Tanawan (Juneau) Chatchadanoraset <tchatc24@uw.edu>
2025-06-12 11:14:10 -0700

    Start plotting phase folded LC

--- analysis.py
+++ analysis.py
@@ -5,7 +5,8 @@ import glob
 import gc
 import matplotlib.pyplot as plt
 
-from astropy.timeseries import LombScargle
+from astropy.timeseries import LombScargle, TimeSeries
+from astropy.time import Time
 import astropy.units as u
 
 import pdb
@@ -36,6 +37,9 @@ def plot_light_curve(times, fluxes):
 
 
 def determine_lc_period(times, fluxes, plot=False):
+    """Calculate the period of the light curve using Lomb-Scargle Periodogram
+    
+    Optionally, produce a plot of the periodogram"""
 
     # Construct the periodogram using LombScargle
     frequency, power = LombScargle(times, fluxes).autopower()
@@ -66,6 +70,34 @@ def determine_lc_period(times, fluxes, plot=False):
     return lc_period
 
 
+def plot_phase_folded(times, fluxes, period):
+    """Plot a phase-folded light curve"""
+
+    # Normalize flux to one
+    fluxes = fluxes / np.max(fluxes)
+
+    # Create a TimeSeries object for phase folding
+    tt = TimeSeries(data=fluxes.reshape(-1, 1), time=Time(times.to(u.day), format='jd'))
+
+    # Phase fold using the primary eclipse as reference point
+    PRIMARY_ECLIPSE_TIME = Time((np.min(times) + 1.25 * u.h).to(u.day), format="jd")
+    folded_tt = tt.fold(period.to(u.day), epoch_time = PRIMARY_ECLIPSE_TIME)
+
+    plot_time = np.array([t.value for t in folded_tt["time"]])
+
+    # Plot
+    fig, ax = plt.subplots(figsize=(8,6))
+
+    ax.scatter(plot_time, folded_tt["col0"])
+    ax.grid(linestyle=":", alpha=0.5)
+    ax.set_xlabel("Phase", fontsize=20)
+    ax.set_xlim(-1, 1)
+    ax.set_ylabel("Relative Flux", fontsize=20)
+    ax.tick_params(axis='both', which='major', labelsize=16)
+    
+    fig.savefig("figures/phase_plot.pdf")
+
+
 if __name__ == "__main__":
 
     # Load the datas
@@ -76,3 +108,5 @@ if __name__ == "__main__":
 
     lc_period = determine_lc_period(times, fluxes, plot=True)
     print(lc_period)
+
+    plot_phase_folded(times, fluxes, lc_period)
